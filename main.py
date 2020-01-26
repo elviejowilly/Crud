5da2335a0d1bb3b1647eceb1f1e74b315acfa465
William Rosales <rosales.william1993@gmail.com>
2020-01-26 15:08:20 -0500

    Agregando esquema CRUD, solucionado problema de comando Update

--- main.py
+++ main.py
@@ -1,8 +1,93 @@
 
 
-clients = 'pablo ,ricardo ,'
+clients = 'pablo,ricardo,'
 
+
+def list_clients():
+    global clients
+    print(clients)
+
+
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        print('Client is not in clients list')
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',' , '')
+    else:
+       print('Client is not in clients list')
+
+
+def search_client(client_name):
+    clients_list = clients.split(',')
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+def _get_client_name():
+    return input('What is the client name?: ')
+
+def create_client(client_name):
+    global clients
+
+    if client_name not in clients:
+        _add_space
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client already exists in clients list')
+
+
+def _add_comma():
+    global clients
+    clients += ','
+
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[L]ist clients')
+    print('[U]pdate client')
+    print('[S]earch client')
+    print('[D]elete client')
 
 if __name__ == '__main__':
-    clients += 'william ,'
-    print(clients)
+    _print_welcome()
+    
+    command = input()
+    command = command.upper()
+    
+    if command == 'C':
+        client_name = _get_client_name()
+        create_client(client_name)
+        list_clients()
+    elif command == 'L':
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name?: ')
+        update_client(client_name, updated_client_name)
+        list_clients()
+    elif command == 'D':
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print('The client {} is in the client list'.format(client_name))
+        else:
+            print('The client {} isnt in the client list'.format(client_name))
